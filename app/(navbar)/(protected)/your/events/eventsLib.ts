bc0ecc888f61f66b75b7430a641d258612eb874a
Medo <martinmederly@gmail.com>
2024-06-18 20:52:14 +0200

    feat: add event type information

--- app/(navbar)/(protected)/your/events/eventsLib.ts
+++ app/(navbar)/(protected)/your/events/eventsLib.ts
@@ -1,80 +1,103 @@
 import { EventType } from '@prisma/client';
 
-type EventFormValue = {
+type EventTypeInfo = {
   name: string;
   rounds: number;
   entry: number;
-  winnings: number;
+  winnings: Record<number, number | undefined>;
 };
 
 const todayDate = new Date();
 const todayDateString = `${todayDate.getFullYear()}-${todayDate.getMonth() + 1}-${todayDate.getDate()}`;
 
-export const formValuesByEventType: Record<EventType, EventFormValue> = {
+export const eventTypeInfo: Record<EventType, EventTypeInfo> = {
   CHALLENGE32: {
     name: `Challenge 32 [${todayDateString}]`,
     rounds: 7,
     entry: 30,
-    winnings: -30,
+    winnings: {},
   },
   CHALLENGE64: {
     name: `Challenge 64 [${todayDateString}]`,
     rounds: 8,
-    entry: 0,
-    winnings: 0,
+    entry: 30,
+    winnings: {},
   },
   FRIENDLY_LEAGUE: {
     name: `Friendly League [${todayDateString}]`,
-    rounds: 0,
-    entry: 0,
-    winnings: 0,
+    rounds: 5,
+    entry: 6,
+    winnings: {
+      0: 0,
+      1: 0,
+      2: 3,
+      3: 7,
+      4: 10,
+      5: 18,
+    },
   },
   LEAGUE: {
     name: `League [${todayDateString}]`,
-    rounds: 0,
-    entry: 0,
-    winnings: 0,
+    rounds: 5,
+    entry: 10,
+    winnings: {
+      0: 0,
+      1: 0,
+      2: 5,
+      3: 12,
+      4: 22,
+      5: 37,
+    },
   },
   OPEN_PLAY: {
     name: `Open Play [${todayDateString}]`,
-    rounds: 0,
+    rounds: 100,
     entry: 0,
-    winnings: 0,
+    winnings: {},
   },
   OTHER: {
     name: `Other Event [${todayDateString}]`,
-    rounds: 0,
+    rounds: 1,
     entry: 0,
-    winnings: 0,
+    winnings: {},
   },
   PRELIMINARY: {
     name: `Preliminary [${todayDateString}]`,
-    rounds: 0,
-    entry: 0,
-    winnings: 0,
+    rounds: 4,
+    entry: 20,
+    winnings: {
+      0: 0,
+      1: 0,
+      2: 12,
+      3: 28,
+      4: 50,
+    },
   },
   RL_BIG: {
     name: `RL Big [${todayDateString}]`,
-    rounds: 0,
+    rounds: 9,
     entry: 0,
-    winnings: 0,
+    winnings: {},
   },
   RL_MEDIUM: {
     name: `RL Medium [${todayDateString}]`,
-    rounds: 0,
+    rounds: 4,
     entry: 0,
-    winnings: 0,
+    winnings: {},
   },
   RL_SMALL: {
     name: `RL Small [${todayDateString}]`,
-    rounds: 0,
+    rounds: 3,
     entry: 0,
-    winnings: 0,
+    winnings: {},
   },
   TWO_PLAYER_QUEUE: {
     name: `Two Player Queue [${todayDateString}]`,
-    rounds: 0,
-    entry: 0,
-    winnings: 0,
+    rounds: 1,
+    entry: 2,
+    winnings: {
+      0: 0.5,
+      1: 3,
+    },
   },
 };
