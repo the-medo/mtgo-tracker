26da7680cdc2b2460c22e12567136a1be4378b53
Medo <martinmederly@gmail.com>
2024-08-28 20:46:36 +0200

    migration: long forgotten migration

---
+++ prisma/migrations/20240715075037_change_is_win_to_result_in_game_table/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isWin` on the `Game` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Game" DROP COLUMN "isWin",
+ADD COLUMN     "result" "MatchResult";
