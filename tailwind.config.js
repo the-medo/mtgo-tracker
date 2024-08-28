95a65c6dae52e98a349b7d9efbe03b78514c6c88
Medo <martinmederly@gmail.com>
2024-08-28 20:46:08 +0200

    chore: version updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  safelist: [{ pattern: /^border-/ }, { pattern: /^bg-/ }],
   theme: {
     extend: {},
   },
