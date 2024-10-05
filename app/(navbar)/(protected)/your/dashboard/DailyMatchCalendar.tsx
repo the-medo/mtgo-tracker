ae7dec0bdf15a6e62a74091f1bea96e8079160fd
Medo <martinmederly@gmail.com>
2024-10-05 20:25:53 +0200

    feat: replace ResponsiveCalendar with ResponsiveTimeRange, improve colors

--- app/(navbar)/(protected)/your/dashboard/DailyMatchCalendar.tsx
+++ app/(navbar)/(protected)/your/dashboard/DailyMatchCalendar.tsx
@@ -2,30 +2,60 @@
 
 import cn from 'classnames';
 import { DailyMatch, useDailyMatches } from '@/app/api/dashboard/daily/matches/useDailyMatches';
-import { CalendarDatum, CalendarTooltipProps, ResponsiveCalendar } from '@nivo/calendar';
-import { useCallback, useMemo, useState } from 'react';
+import { CalendarDatum, CalendarTooltipProps, ResponsiveTimeRange } from '@nivo/calendar';
+import { useCallback, useMemo} from 'react';
 import DailyMatchCalendarTooltip from '@/app/(navbar)/(protected)/your/dashboard/DailyMatchCalendarTooltip';
 import Title from '@/components/typography/Title';
 import { Chip } from '@nextui-org/chip';
+import useStore from '@/store/store';
+import { format, subMonths } from 'date-fns';
+import { TimeRangeSvgProps } from '@nivo/calendar/dist/types/types';
 
 export type DailyMatchCalendarData = CalendarDatum & DailyMatch;
 
 interface DailyMatchCalendarProps {}
 
 export default function DailyMatchCalendar({}: DailyMatchCalendarProps) {
+  const breakpoint = useStore(state => state.breakpoint);
   const { data: dailyMatches } = useDailyMatches();
-  const today = new Date();
-  const [startDate, setStartDate] = useState<string>(`${today.getFullYear()}-01-01`);
 
-  const data: DailyMatchCalendarData[] = useMemo(
-    () =>
-      (dailyMatches ?? []).map(d => ({
-        ...d,
-        value: d.matchDraws + d.matchWins - d.matchLosses,
-        day: d.matchDate.slice(0, 10),
-      })),
-    [dailyMatches],
-  );
+  const { startDate, endDate, weekdayTicks, weekdayLegendOffset } = useMemo(() => {
+    const isSmall = ['xs', 'sm'].includes(breakpoint);
+
+    const today = new Date();
+    const prevYear = format(subMonths(today, 6), 'yyyy-MM-dd');
+    const prevThreeMonths = format(subMonths(today, 3), 'yyyy-MM-dd');
+    const weekdayTicks: TimeRangeSvgProps['weekdayTicks'] = isSmall ? [] : [0, 2, 4, 6];
+
+    return {
+      startDate: isSmall ? prevThreeMonths : prevYear,
+      endDate: today,
+      weekdayTicks,
+      weekdayLegendOffset: isSmall ? 0 : 75,
+    };
+  }, [breakpoint]);
+
+  const data: DailyMatchCalendarData[] = useMemo(() => {
+    const matches = dailyMatches ?? [];
+    const computeValue = (d: DailyMatch) => d.matchDraws + d.matchWins - d.matchLosses;
+
+    let maxValue = Infinity,
+      minValue = Infinity;
+    matches.forEach(d => {
+      const value = computeValue(d);
+      if (value > maxValue || maxValue === Infinity) maxValue = value;
+      if (value < minValue) minValue = value;
+    });
+
+    const minusRatio = minValue >= 0 ? 1 : (maxValue / minValue) * -1;
+    const diff = minValue >= 0 ? 0 : minValue * minusRatio * -1;
+
+    return matches.map(d => ({
+      ...d,
+      value: (computeValue(d) < 0 ? computeValue(d) * minusRatio : computeValue(d)) + diff,
+      day: d.matchDate.slice(0, 10),
+    }));
+  }, [dailyMatches]);
 
   const total = useMemo(
     () =>
@@ -40,15 +70,15 @@ export default function DailyMatchCalendar({}: DailyMatchCalendarProps) {
     [dailyMatches],
   );
 
-  const tooltip = useCallback(
-    (n: CalendarTooltipProps & { data?: DailyMatchCalendarData }) =>
-      n.data ? <DailyMatchCalendarTooltip data={n.data as DailyMatchCalendarData} /> : null,
-    [],
-  );
+  const tooltip = useCallback((n: CalendarTooltipProps) => {
+    return n ? (
+      <DailyMatchCalendarTooltip data={n as CalendarTooltipProps & DailyMatchCalendarData} />
+    ) : null;
+  }, []);
 
   return (
     <>
-      <div className={cn(`flex flex-col h-[200px] w-1/2 min-w-[330px] gap-2`)}>
+      <div className={cn(`flex flex-col h-[200px] grow min-w-[330px] gap-2`)}>
         <div className="flex flex-row justify-between">
           <Title title="Matches - 2024" />
           {dailyMatches ? (
@@ -57,17 +87,20 @@ export default function DailyMatchCalendar({}: DailyMatchCalendarProps) {
             </Chip>
           ) : null}
         </div>
-        <ResponsiveCalendar
+        <ResponsiveTimeRange
           from={startDate}
-          to={today}
+          to={endDate}
           data={data}
           emptyColor="#eeeeee"
-          colors={['#f47560', '#e8c1a0', '#97e3d5', '#61cdbb']}
-          yearSpacing={40}
-          monthBorderColor="#ffffff"
+          colors={['#f47560', '#f47560', '#e8c1a0', '#f4ec97', '#97e3d5', '#61cdbb']}
+          margin={{ top: 20 }}
           dayBorderWidth={2}
           tooltip={tooltip}
           dayBorderColor="#ffffff"
+          monthLegendOffset={10}
+          firstWeekday="monday"
+          weekdayTicks={weekdayTicks}
+          weekdayLegendOffset={weekdayLegendOffset}
         />
       </div>
     </>
--- app/(navbar)/(protected)/your/dashboard/EventAggregations.tsx
+++ app/(navbar)/(protected)/your/dashboard/EventAggregations.tsx
@@ -10,7 +10,9 @@ export default function EventAggregations({}: EventAggregationsProps) {
   const { data: aggregates } = useEventAggregates();
 
   return (
-    <div className={cn(`flex flex-col min-w-[300px] h-[200px] w-full md:w-1/3 gap-2`)}>
+    <div
+      className={cn(`flex flex-col min-w-[300px] pt-4 md:pt-0 md:h-[200px] w-full md:w-1/4 gap-2`)}
+    >
       <Title title="Event statistics" />
       <div className={cn(`flex flex-col p-2 md:p-4 w-full gap-2 rounded-md bg-default-100`)}>
         <div className="flex flex-row gap-2 md:gap-4">
